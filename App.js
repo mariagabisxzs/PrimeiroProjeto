7f652ffe3865311e34f836e50c665d261c27b363
Mariagabisxzs <Mariagabrielagomes923@gmail.com>
2024-03-14 10:34:40 -0300

    Novo botão

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Maria Gomes</Text>
       <StatusBar style="auto" />
+    <Button title='Salvar'></Button>
     </View>
   );
 }
